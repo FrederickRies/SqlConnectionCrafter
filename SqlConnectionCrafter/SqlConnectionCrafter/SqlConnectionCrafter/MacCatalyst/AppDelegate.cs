23072b1470739f3b8030f8d1253dafa84be9b563
Fred <17268332+FrederickRies@users.noreply.github.com>
2021-05-31 23:13:43 +0200

    MAUI project initialization.

---
+++ SqlConnectionCrafter/SqlConnectionCrafter/SqlConnectionCrafter/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace SqlConnectionCrafter
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
